fc9968173bfb75f18ce39360ee3a55d9c3283ce1
burak <burakaydin9761@gmail.com>
2022-12-22 12:28:08 +0300

    Design Recent Search section

--- lib/screens/new_home_screen.dart
+++ lib/screens/new_home_screen.dart
@@ -1,57 +1,114 @@
 import 'package:fabrikod_quran/constants/constants.dart';
 import 'package:fabrikod_quran/providers/home_provider.dart';
 import 'package:fabrikod_quran/providers/quran_provider.dart';
+import 'package:fabrikod_quran/widgets/cards/grid_card.dart';
 import 'package:fabrikod_quran/widgets/juz_category_list_widget.dart';
 import 'package:fabrikod_quran/widgets/cards/surah_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
-          child: buildJuzCategoryList(context),
+          child: buildRecentAndJuzCategoryList(),
         ),
       ),
     );
   }
 
+  Widget buildRecentAndJuzCategoryList() {
+    return Column(
+      children: [
+        buildResent(),
+        buildJuzCategoryList(),
+      ],
+    );
+  }
+
+  /// Build Resend
+  Widget buildResent() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kPaddingL),
+          child: Text(
+            context.translate.recent,
+            style: context.theme.textTheme.displayLarge?.copyWith(letterSpacing: 0.04),
+          ),
+        ),
+        const SizedBox(height: kPaddingL),
+        SizedBox(
+          height: 90,
+          child: GridView.builder(
+            itemCount: 10,
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.symmetric(horizontal: kPaddingM),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 1,
+              mainAxisSpacing: kPaddingS,
+            ),
+            itemBuilder: (context, index) {
+              return GridCard(
+                text: index.toString(),
+                onTap: () {},
+              );
+            },
+          ),
+        ),
+        const SizedBox(height: kPaddingXL),
+      ],
+    );
+  }
+
   /// List of The Juz Category
-  Widget buildJuzCategoryList(BuildContext context) {
-    return JuzCategoryListWidget(
-      juzList: context.watch<QuranProvider>().juzList,
-      listType: context.watch<HomeProvider>().juzListType,
-      onChangedListType: context.read<HomeProvider>().changeJuzListType,
-      onTapGridCard: (int juzId) {
-        print("On Tap Juz Card : $juzId");
-      },
-      onTapSurahCard: (int surahId) {
-        print("On Tap Surah Card : $surahId");
-      },
+  Widget buildJuzCategoryList() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kPaddingL),
+      child: JuzCategoryListWidget(
+        juzList: context.watch<QuranProvider>().juzList,
+        listType: context.watch<HomeProvider>().juzListType,
+        onChangedListType: context.read<HomeProvider>().changeJuzListType,
+        onTapGridCard: (int juzId) {
+          print("On Tap Juz Card : $juzId");
+        },
+        onTapSurahCard: (int surahId) {
+          print("On Tap Surah Card : $surahId");
+        },
+      ),
     );
   }
 
   /// List of the Surah Verses
-  Widget buildSurahList(BuildContext context) {
+  Widget buildSurahList() {
     var surahs = context.watch<QuranProvider>().surahs;
-    return ListView.separated(
-      itemCount: surahs.length,
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemBuilder: (context, index) {
-        final surah = surahs[index];
-        return SurahCard(
-          surahModel: surah,
-          onTap: () {
-            print("On Tap Surah Card : ${surah.id}");
-          },
-        );
-      },
-      separatorBuilder: (context, index) => const SizedBox(height: kPaddingL),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kPaddingL),
+      child: ListView.separated(
+        itemCount: surahs.length,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (context, index) {
+          final surah = surahs[index];
+          return SurahCard(
+            surahModel: surah,
+            onTap: () {
+              print("On Tap Surah Card : ${surah.id}");
+            },
+          );
+        },
+        separatorBuilder: (context, index) => const SizedBox(height: kPaddingL),
+      ),
     );
   }
 }
