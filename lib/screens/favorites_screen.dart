61f0fc8a264b9dfa0467375d125a84e2a438e256
yusufilhamcetinkaya <yusuficetinkaya@gmail.com>
2022-12-23 10:25:15 +0300

    favorites_screen , favorites_card and new_verse_card created

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,12 +1,11 @@
 import 'package:fabrikod_quran/constants/constants.dart';
 import 'package:fabrikod_quran/models/verse_model.dart';
 import 'package:fabrikod_quran/providers/favorites_provider.dart';
-import 'package:fabrikod_quran/widgets/app_bars/main_app_bar.dart';
-import 'package:fabrikod_quran/widgets/cards/verse_card.dart';
+import 'package:fabrikod_quran/widgets/cards/favorites_card.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../widgets/no_item_widget.dart';
+import '../widgets/app_bars/primary_app_bar.dart';
 
 class FavoritesScreen extends StatefulWidget {
   const FavoritesScreen({super.key});
@@ -19,33 +18,52 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MainAppBar(title: context.translate.favorites),
+      appBar: PrimaryAppBar(title: context.translate.theOpenQuran),
       body: buildBody,
     );
   }
 
   Widget get buildBody {
-    List<VerseModel> favoriteVerses = context.watch<FavoritesProvider>().favoriteVerses;
-    return favoriteVerses.isEmpty
-        ? const NoItemWidget(
-            text: "No Favorites Added",
-            icon: Icon(
-              Icons.favorite_border_outlined,
-              size: 50,
-            ))
-        : ListView.separated(
-            itemCount: favoriteVerses.length,
+    List<VerseModel> favoriteVerses =
+        context.watch<FavoritesProvider>().favoriteVerses;
+    // return favoriteVerses.isEmpty
+    //     ? const NoItemWidget(
+    //         text: "No Favorites Added",
+    //         icon: Icon(
+    //           Icons.favorite_border_outlined,
+    //           size: 50,
+    //           color: AppColors.grey,
+    //         ))
+    //     :
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.fromLTRB(kPaddingXL, kPaddingL, 0, 0),
+            child: Text(
+              'Favorites',
+              style: context.theme.textTheme.displayLarge,
+            ),
+          ),
+          ListView.separated(
+            itemCount: 10, //favoriteVerses.length,
             shrinkWrap: true,
             primary: false,
             padding: const EdgeInsets.symmetric(
-              vertical: kPaddingXXL,
+              vertical: kPaddingL,
               horizontal: kPaddingXL,
             ),
-            itemBuilder: (context, index) => VerseCard(
-              verseModel: favoriteVerses.elementAt(index),
-              isFavorite: true,
+            itemBuilder: (context, index) => const FavoritesCard(
+              surahName: 'Al - Fatihah',
+              surahNameTranslation: 'The Opener',
+              pageNumber: 254,
             ),
-            separatorBuilder: (context, index) => const SizedBox(height: kPaddingXL),
-          );
+            separatorBuilder: (context, index) =>
+                const SizedBox(height: kPaddingXL),
+          ),
+        ],
+      ),
+    );
   }
 }
