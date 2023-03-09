659a7786b5d7332b0c2f9855919f709d6fe1ea04
yusufilhamcetinkaya <yusuficetinkaya@gmail.com>
2023-03-09 18:04:23 +0300

    Changed widgetSpan to textSpan on quran_page_widget

--- lib/widgets/quran/quran_page_widget.dart
+++ lib/widgets/quran/quran_page_widget.dart
@@ -4,9 +4,16 @@ import 'package:fabrikod_quran/models/surah_model.dart';
 import 'package:fabrikod_quran/models/verse_model.dart';
 import 'package:fabrikod_quran/providers/quran_provider.dart';
 import 'package:fabrikod_quran/widgets/basmala_title.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../pop_up/verse_pop_up_menu.dart';
+
+import '../../models/bookmark_model.dart';
+import '../../providers/bookmark_provider.dart';
+import '../../providers/favorites_provider.dart';
+import '../../providers/player_provider.dart';
+import '../../providers/surah_details_provider.dart';
+import '../cards/verse_menu_item.dart';
 
 class QuranPageWidget extends StatelessWidget {
   const QuranPageWidget({
@@ -48,8 +55,7 @@ class QuranPageWidget extends StatelessWidget {
         return Column(
           children: [
             BasmalaTitle(verseKey: verses.first.verseKey ?? ""),
-            buildVersesText(context, verses, textScaleFactor, layoutOptions,
-                fontTypeArabic),
+            buildVersesText(context, verses, textScaleFactor, layoutOptions, fontTypeArabic),
           ],
         );
       },
@@ -67,46 +73,137 @@ class QuranPageWidget extends StatelessWidget {
     return RichText(
       key: globalKey,
       textDirection: TextDirection.rtl,
-      textAlign: layoutOptions == ELayoutOptions.justify
-          ? TextAlign.justify
-          : TextAlign.right,
+      textAlign: layoutOptions == ELayoutOptions.justify ? TextAlign.justify : TextAlign.right,
       textScaleFactor: textScaleFactor,
       text: TextSpan(
         style: context.theme.textTheme.headlineLarge?.copyWith(
             height: 2.4,
             fontFamily: Fonts.getArabicFont(fontTypeArabic),
-            color: context
-                .watch<QuranProvider>()
-                .surahDetailsPageThemeColor
-                .textColor),
+            color: context.watch<QuranProvider>().surahDetailsPageThemeColor.textColor),
         children: verses
             .map((e) {
               return [
-                WidgetSpan(
-                  child: VersePopUpMenu(
-                    globalKey: globalKey,
-                    verseModel: e,
-                    isPlaying: false,
-                    playFunction: (verse, isPlay) {},
-                    isFavorite: false,
-                    favoriteFunction: (verseModel, isFavorite) {},
-                    isBookmark: false,
-                    bookmarkFunction: (bookMarkType, verseModel, isBookmark) {},
-                    shareFunction: (p0) {},
-                    changeSelectedVerseKey: (selectedVerseKey) {},
-                    child: Text(e.text!),
+                // WidgetSpan(
+                //   style: context.theme.textTheme.headlineLarge?.copyWith(
+                //       height: 2.4,
+                //       fontFamily: Fonts.getArabicFont(fontTypeArabic),
+                //       color: context.watch<QuranProvider>().surahDetailsPageThemeColor.textColor),
+                //   child: VersePopUpMenu(
+                //     globalKey: globalKey,
+                //     verseModel: e,
+                //     isPlaying: context.watch<PlayerProvider>().isPlayingVerse(e.verseKey ?? ""),
+                //     playFunction: (verse, isPlaying) {
+                //       context.read<SurahDetailsProvider>().onTapVerseCardPlayOrPause(
+                //             e.verseNumber! - 1,
+                //             isPlaying,
+                //           );
+                //     },
+                //     isFavorite: context.watch<FavoritesProvider>().isFavoriteVerse(e),
+                //     favoriteFunction: context.read<FavoritesProvider>().onTapFavoriteButton,
+                //     isBookmark: context.watch<BookmarkProvider>().isBookmark(
+                //           BookMarkModel(bookmarkType: EBookMarkType.verse, verseModel: e),
+                //         ),
+                //     bookmarkFunction: context.read<BookmarkProvider>().onTapBookMarkButton,
+                //     shareFunction: (verseModel) {
+                //       context.read<SurahDetailsProvider>().shareVerse(verseModel, e.verseNumber!);
+                //     },
+                //     changeSelectedVerseKey: context.read<SurahDetailsProvider>().changeSelectedVerseKey,
+                //     child: Text(
+                //       e.text!,
+                //       textDirection: TextDirection.rtl,
+                //       style: context.theme.textTheme.headlineLarge?.copyWith(
+                //           height: 2.4,
+                //           fontFamily: Fonts.getArabicFont(fontTypeArabic),
+                //           color: context.watch<QuranProvider>().surahDetailsPageThemeColor.textColor),
+                //     ),
+                //   ),
+                // ),
+                TextSpan(
+                  text: e.text!,
+                  style: context.theme.textTheme.headlineLarge?.copyWith(
+                    fontFamily: Fonts.uthmanic,
+                    fontSize: 27,
+                    height: 0,
+                    color: context.watch<QuranProvider>().surahDetailsPageThemeColor.textColor,
                   ),
+                  recognizer: LongPressGestureRecognizer()
+                    ..onLongPress = () async {
+                      context.read<SurahDetailsProvider>().changeSelectedVerseKey(e.verseKey);
+                      RenderBox box = context.findRenderObject() as RenderBox;
+                      Offset position = box.localToGlobal(Offset.zero);
+                      await showMenu(
+                        context: context,
+                        color: context.theme.cardColor,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: AppColors.white.withOpacity(0.1),
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        position: RelativeRect.fromRect(
+                          Rect.fromLTWH(
+                              position.dx + kSizeM, position.dy + (box.paintBounds.size.height - 200 - kSizeM), 0, 0),
+                          Rect.fromLTWH(0, 0, box.paintBounds.size.width, box.paintBounds.size.height),
+                        ),
+                        items: [
+                          PopupMenuItem(
+                            onTap: () => (verse, isPlaying) {
+                              context.read<SurahDetailsProvider>().onTapVerseCardPlayOrPause(
+                                    e.verseNumber! - 1,
+                                    isPlaying,
+                                  );
+                            },
+                            child: VerseMenuItem(
+                              iconPath: context.read<PlayerProvider>().isPlayingVerse(e.verseKey ?? "")
+                                  ? ImageConstants.pauseIcon
+                                  : ImageConstants.play,
+                              buttonName: context.read<PlayerProvider>().isPlayingVerse(e.verseKey ?? "")
+                                  ? context.translate.pause
+                                  : context.translate.play,
+                            ),
+                          ),
+                          PopupMenuItem(
+                            onTap: () => context.read<FavoritesProvider>().onTapFavoriteButton,
+                            child: VerseMenuItem(
+                              iconPath: context.read<FavoritesProvider>().isFavoriteVerse(e)
+                                  ? ImageConstants.favoriteActiveIcon
+                                  : ImageConstants.favoriteInactiveIcon,
+                              buttonName: context.translate.favorite,
+                            ),
+                          ),
+                          PopupMenuItem(
+                            onTap: () => context.read<BookmarkProvider>().onTapBookMarkButton,
+                            child: VerseMenuItem(
+                              iconPath: context.read<BookmarkProvider>().isBookmark(
+                                        BookMarkModel(bookmarkType: EBookMarkType.verse, verseModel: e),
+                                      )
+                                  ? ImageConstants.bookmarkActiveIcon
+                                  : ImageConstants.bookmarkInactiveIcon,
+                              buttonName: context.translate.bookmark,
+                            ),
+                          ),
+                          PopupMenuItem(
+                            onTap: () => (verseModel) {
+                              context.read<SurahDetailsProvider>().shareVerse(verseModel, e.verseNumber!);
+                            },
+                            child: VerseMenuItem(
+                              iconPath: ImageConstants.shareAppIcon,
+                              buttonName: context.translate.share,
+                            ),
+                          ),
+                        ],
+                      );
+                      context.read<SurahDetailsProvider>().changeSelectedVerseKey(null);
+                    },
                 ),
                 TextSpan(
                   text: Utils.getArabicVerseNo(e.verseNumber.toString()),
                   style: context.theme.textTheme.headlineLarge?.copyWith(
-                      fontFamily: Fonts.uthmanic,
-                      fontSize: 27,
-                      height: 0,
-                      color: context
-                          .watch<QuranProvider>()
-                          .surahDetailsPageThemeColor
-                          .textColor),
+                    fontFamily: Fonts.uthmanic,
+                    fontSize: 27,
+                    height: 0,
+                    color: context.watch<QuranProvider>().surahDetailsPageThemeColor.textColor,
+                  ),
                 ),
               ];
             })
@@ -128,15 +225,13 @@ class QuranPageWidget extends StatelessWidget {
         children: [
           Text(
             "${context.translate.juz} ${verse.juzNumber} | ${context.translate.hizb} ${verse.hizbNumber} - ${context.translate.page} ${verse.pageNumber}",
-            style: context.theme.textTheme.bodySmall?.copyWith(
-                color: surahDetailsPageTheme.transparentTextColor,
-                letterSpacing: 0.15),
+            style: context.theme.textTheme.bodySmall
+                ?.copyWith(color: surahDetailsPageTheme.transparentTextColor, letterSpacing: 0.15),
           ),
           Text(
             verse.pageNumber?.quranPageNumber ?? "",
-            style: context.theme.textTheme.bodyMedium?.copyWith(
-                color: surahDetailsPageTheme.transparentVectorColor,
-                letterSpacing: 0.04),
+            style: context.theme.textTheme.bodyMedium
+                ?.copyWith(color: surahDetailsPageTheme.transparentVectorColor, letterSpacing: 0.04),
           ),
         ],
       ),
